e92b69ebaa93329a9f0d082474262b14d4280ac9
joshhoneypot <joshuaangugo@gmail.com>
2023-05-17 11:22:26 +0300

    Lab done

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,24 @@
 def starts_with_a_vowel?(word)
-
+    pattern = /\A[aeiou]/i
+    !!(pattern.match(word))
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
-end
+    pattern = /\Aun\w+ing\z/i
+    text.scan(/\b\w+\b/).select { |word| pattern.match?(word) }
+end  
 
 def words_five_letters_long(text)
-
+    pattern = /\b\w{5}\b/
+    text.scan(pattern)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    pattern = /\A[A-Z].*[[:punct:]]\z/
+    !!(pattern.match(text))
 end
 
 def valid_phone_number?(phone)
-
+    pattern = /^\+?[\d\s()-]+$/
+    !!(pattern.match(phone))
 end
